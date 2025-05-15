c8dd2262743f3f84c0034f1774437f308e4142be
Jim Labilles <jim@labilles.com.au>
2025-05-15 15:39:06 +0930

    update to code analysis

--- Documentation/sqlCalls.sql
+++
@@ -1,2 +0,0 @@
---Calling first image for product category page.
-SELECT imageRef FROM product, productimage WHERE product.prodID = productimage.prodID AND category = "Jedi" AND imageRef LIKE "%1%";
--- scripts/myscript.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  docume
-  nt.getElementById("main").style.marginLeft = "0";
-}
